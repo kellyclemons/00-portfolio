02cfdf3801cf783de3b66ba7f7b6c519a3b65a0b
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-03-22 13:50:51 -0500

    setup basic application

---
+++ app/routes/application.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div class="">
+
+    <nav class="navbar">
+  <div class="navbar-left">
+    <img class="navbar-logo" src="http://i64.tinypic.com/210glc4.gif" alt="">
+    <p class="navbar-title">Kelly Clemons</p>
+  </div>
+  <div class="navbar-right">
+    <a href="#" class="navbar-right-item">Portfolio</a>
+    <a href="#" class="navbar-right-item">Blog</a>
+    <a href="#" class="navbar-right-item">City</a>
+  </div>
+</nav>
+
+  </div>
+</template>
+
+<script>
+import Index from './index.vue';
+export default {
+  components: {
+    Index,
+  },
+
+  name: 'Application',
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
