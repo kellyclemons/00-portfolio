808bf33c94642421b41ae719a18e0822bb9872a2
Kelly Clemons <kellyclemons@Kellys-MacBook-Air.local>
2017-03-29 14:45:30 -0500

    hover effects

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,17 +1,7 @@
 <template lang="html">
   <div class="">
 
-    <nav class="navbar">
-  <div class="navbar-left">
-    <router-link :to="{ name: 'index' }"><img class="navbar-logo" src="http://i64.tinypic.com/210glc4.gif" alt=""></router-link>
-    <p class="navbar-title">Kelly Clemons</p>
-  </div>
-  <div class="navbar-right">
-    <router-link :to="{ name: 'portfolio' }" class="navbar-right-item">Portfolio</router-link>
-    <router-link :to="{ name: 'blog' }" class="navbar-right-item">Blog</router-link>
-    <router-link :to="{ name: 'city' }" class="navbar-right-item">City</router-link>
-  </div>
-</nav>
+
 
     <router-view></router-view>
 
--- app/routes/index.vue
+++ app/routes/index.vue
@@ -1,7 +1,22 @@
 <template lang="html">
   <div class="">
 
+
+
   <div class="top">
+
+    <nav class="navbar has-scrolled">
+      <div class="navbar-left">
+        <router-link :to="{ name: 'index' }"><img class="navbar-logo" src="http://i64.tinypic.com/210glc4.gif" alt=""></router-link>
+        <p class="navbar-title">Kelly Clemons</p>
+      </div>
+      <div class="navbar-right">
+        <router-link :to="{ name: 'portfolio' }" class="navbar-right-item">Portfolio</router-link>
+        <router-link :to="{ name: 'blog' }" class="navbar-right-item">Blog</router-link>
+        <router-link :to="{ name: 'city' }" class="navbar-right-item">City</router-link>
+      </div>
+    </nav>
+
     <div class="main-heading">
       <!-- <img src="http://stabmag.com/assets/Uploads/2014/03/Taj_Burrow_JOHN_Barton.jpg" alt="" class="background-pic"> -->
       <div class="main-heading-text">
